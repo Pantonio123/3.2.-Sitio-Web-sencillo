d4697c20280ee173dc126a311e25e7e3d0803586
Pantonio123 <paulo.iniguez3223@alumnos.udg.mx>
2025-05-17 18:53:23 -0600

    corrigiendo errores

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
 
 Route::get('/movies', [MovieController::class, 'index']); // Obtener todas las películas
-Route::get('/movies/{id}', [MovieController::class, 'show']); // Obtener película por ID
+Route::get('/movies/{id}', [MovieController::class, 'index']); // Obtener película por ID
 Route::post('/movies', [MovieController::class, 'store']); // Insertar una nueva película
 Route::put('/movies/{id}', [MovieController::class, 'update']); // Modificar un registro (PUT)
 Route::delete('/movies/{id}', [MovieController::class, 'destroy']); // Eliminar un registro (DELETE)
